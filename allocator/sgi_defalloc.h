0ce3efe9aa76c599b297711dc923138a480066d6
fanwenyao <418452307@qq.com>
2020-08-20 08:54:17 -0700

    implemetation allocator

--- allocator/sgi_defalloc.h
+++ allocator/sgi_defalloc.h
@@ -3,8 +3,28 @@
 
 #include <new>
 #include <iostream>
+#include <cstddef>
+#include <cstdlib>
+#include <algorithm>
+#include <climits>
 
 namespace sgi {
+
+template<typename T>
+inline T* allocate(ptrdiff_t size, T*) {
+    set_new_handler(0);
+    T* tmp = ::operator new(size * sizeof(T));
+    if (tmp == nullptr) {
+        std::cout << "alllocte error" << std::endl;
+        exit(0);
+    }
+    return tmp;
+}
+
+template<typename T>
+inline void deallocate(T* buffer) {
+    ::operator delete(buffer);
+}
 
 template<typename T>
 class allocator {
@@ -15,6 +35,30 @@ public:
     typedef T& reference;
     typedef const T& const_reference;
     typedef ptrdiff_t difference_type;
+    typedef size_t size_type;
+
+    pointer allocate(size_type n) {
+        return ::allocate(n, pointer(0));
+    }
+    void deallocate(pointer p) {
+        ::deallocate(p);
+    }
+
+
+    pointer address(reference x) {
+        return &x;
+    }
+
+    const_pointer const_address(const_reference x) {
+        return &x;
+    }
+
+    size_type init_page_size() {
+        return std::max(size_type(1), size_type(4096/sizeof(T));
+    }
+    size_type max_size() {
+        return std::max(size_type(1), size_type(UINT_MAX/sizeof(T)));
+    }
 };
 
 } // namespace sgi
